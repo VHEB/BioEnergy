9546bc9f5b178060912562b13afc9530de88c541
VHEB <vheb0316@gmail.com>
2025-04-17 11:18:21 -0300

    feat: importação da api de dados

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Usina(models.Model):
+    nome = models.CharField(max_length=200)
+    estado = models.CharField(max_length=2)
+    fonte = models.CharField(max_length=50)
+    potencia_kw = models.FloatField()
+
+    def __str__(self):
+        return self.nome
